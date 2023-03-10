190d83a0ba2831675fde28f2de04af6bf04ac57e
Alexander Zhidkov <azhidkov@gmail.com>
2023-03-10 17:44:32 +0800

    add angularfire

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'coffeevansco',
+    appId: '1:1027930803691:web:abe9ff62dc60edf714ddae',
+    storageBucket: 'coffeevansco.appspot.com',
+    apiKey: 'AIzaSyDOJ4xWJE8NREniDjscNRe7Urgl8YD0lXQ',
+    authDomain: 'coffeevansco.firebaseapp.com',
+    messagingSenderId: '1027930803691',
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
